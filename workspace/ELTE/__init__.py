2a157f01606e1fb5f389c1aeb7d51f643231c1d7
Bence Dudás <52243953+Dudgit@users.noreply.github.com>
2022-05-08 17:04:48 +0200

    Add files via upload

--- workspace/ELTE/__init__.py
+++ workspace/ELTE/__init__.py
@@ -1,6 +1,6 @@
 import sys
 sys.path.append("../src/")
-from reader1 import readRDF
+from reader1 import readRDF, ionForce
 from potentialviz import *
 from calculations import *
 from spce_vars import *
